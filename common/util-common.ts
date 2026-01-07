9519e45914c5cb75a984b60f244be52b8908e207
undyed <Undyed.github@qaq.site>
2026-01-08 00:55:16 +0800

    fix: add .js extension to dayjs plugin imports for ESM compatibility

--- common/util-common.ts
+++ common/util-common.ts
@@ -6,9 +6,9 @@ import { DotenvParseOutput } from "dotenv";
 
 // Init dayjs
 import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone";
-import utc from "dayjs/plugin/utc";
-import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone.js";
+import utc from "dayjs/plugin/utc.js";
+import relativeTime from "dayjs/plugin/relativeTime.js";
 // @ts-ignore
 import { replaceVariablesSync } from "@inventage/envsubst";
 
